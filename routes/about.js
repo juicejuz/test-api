fb8ec2f3d098d06cf5393c61c0f2bfa2c524e16f
dage <dgeiss438@gmail.com>
2019-09-23 11:58:26 +0200

    Update about.js

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ name: 'OAVKDTV' });
+  res.json({ name: "OAVKDTV" });
 });
 // ein paar kommentare
 module.export = router;
